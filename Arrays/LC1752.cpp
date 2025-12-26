e8987727b27ff1a853ab22867b0e842f20a98673
charansaiyalla <charanysai1307@gmail.com>
2025-12-26 08:10:27 +0530

    Added Check if array is sorted and rotated

---
+++ Arrays/LC1752.cpp
@@ -0,0 +1,14 @@
+bool check(int* nums, int numsSize) {
+    int count=0;
+    for(int i=0;i<numsSize-1;i++){
+        if(nums[i]<=nums[i+1]){
+
+        }else{
+            count++;
+        }
+    }
+     if(nums[numsSize - 1] > nums[0]) {
+        count++;
+    }
+    return count<=1;
+}
